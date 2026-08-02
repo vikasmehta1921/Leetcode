e55454fa7472ea2032ec3bc46515bac99fc47c3a
vikasmehta1921 <vikasmehta1921@gmail.com>
2026-08-02 11:19:42 +0530

    Time: 0 ms (100%), Space: 52.5 MB (25.64%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int ans = 0 ;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(ans < i){
+                return false;
+            }
+            ans =  max(ans , nums[i]+i);
+        }
+        return true;
+    }
+};
