a7e45e1f53f53d80a9acd3546c664346db12844c
vikasmehta1921 <vikasmehta1921@gmail.com>
2026-08-05 23:36:14 +0530

    Time: 4 ms (87.09%), Space: 22.8 MB (85.49%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -11,18 +11,15 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1; // min 1 can eat
         int high = *max_element(piles.begin(), piles.end());
-        int ans = INT_MAX;
-        long long totalhour = 0;
+        
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            totalhour = fun(piles, mid);
-            if (totalhour <= h) { // possible ans
-                ans = mid;
+            if (fun(piles, mid) <= h) { // possible ans
                 high = mid -1;
             } else {
                 low = mid+1;
             }
         }
-        return ans;
+        return low;
     }
 };
